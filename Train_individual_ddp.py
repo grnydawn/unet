8114b67d507c81455cd6c5bf626267c2eae6f2fd
Youngsung Kim <youngsung.kim.act2@gmail.com>
2025-05-06 08:38:12 -0400

    fix pickle filename issue

--- Train_individual_ddp.py
+++ Train_individual_ddp.py
@@ -661,6 +661,9 @@ def main():
     
     event("TRAINING BEGIN")
 
+    # memory snapshot
+    torch.cuda.memory._record_memory_history()
+
     # Set device BEFORE initializing process group
     #torch.cuda.set_device(local_rank)
     #device = torch.device(f'cuda:{local_rank}')
@@ -1025,6 +1028,9 @@ def main():
         logger.info("\nIndividual Lead Time Training and Evaluation Complete!")
         logger.info(f"Trained {len(lead_times)} separate models, one for each lead time.")
     
+    # memory snapshot
+    torch.cuda.memory._dump_snapshot(f"{outdir}/unet_memory_{world_rank}.pickle")
+
     # Clean up distributed process group
     dist.destroy_process_group()
 
